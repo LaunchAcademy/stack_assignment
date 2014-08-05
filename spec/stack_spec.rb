5081deae635f857a1d4fc3d7e1322cca29869250
Richard Davis <rdavis.bacs@gmail.com>
2014-08-05 15:22:33 -0400

    use describe blocks

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,14 +3,16 @@ require_relative '../lib/stack'
 describe Stack do
   subject(:stack) { Stack.new }
 
-  it 'allows us to push in objects' do
-    stack.push(:plate)
-    stack.push("Fortune Cookie")
-    stack.push(Time.now)
-    stack.push(3.14159)
+  describe '#push' do
+    it 'allows us to push in objects' do
+      stack.push(:plate)
+      stack.push("Fortune Cookie")
+      stack.push(Time.now)
+      stack.push(3.14159)
+    end
   end
 
-  context '#empty?' do
+  describe '#empty?' do
     it 'returns true when empty' do
       expect(stack.empty?).to eq(true)
     end
@@ -21,20 +23,24 @@ describe Stack do
     end
   end
 
-  it 'lets us see the topmost item' do
-    stack.push(128)
-    expect(stack.peek).to eq(128)
-    expect(stack.empty?).to eq(false)
+  describe '#peek' do
+    it 'lets us see the topmost item' do
+      stack.push(128)
+      expect(stack.peek).to eq(128)
+      expect(stack.empty?).to eq(false)
+    end
   end
 
-  it 'removes and returns the topmost item' do
-    stack.push(42)
-    stack.push(99)
-    stack.push(105)
-    expect(stack.pop).to eq(105)
-  end
+  describe '#pop' do
+    it 'removes and returns the topmost item' do
+      stack.push(42)
+      stack.push(99)
+      stack.push(105)
+      expect(stack.pop).to eq(105)
+    end
 
-  it 'raises an exception if stack is empty' do
-    expect { stack.pop }.to raise_error(StackUnderflow)
+    it 'raises an exception if stack is empty' do
+      expect { stack.pop }.to raise_error(StackUnderflow)
+    end
   end
 end
